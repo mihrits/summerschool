5250d0a170cb7350c42ffc30361fdeb7f9fda3d8
Moorits Mihkel Muru <mooritsmihkel@gmail.com>
2019-06-28 10:19:25 +0300

    Update on parallel hello world

--- mpi/hello-world/hello.f90
+++ mpi/hello-world/hello.f90
@@ -6,8 +6,9 @@ program helloworld
   integer :: rc, rank, size
 
   call mpi_init(rc)
-  call mpi_comm_rank(MPI_COMM_WORLD, rank)
-  call mpi_comm_size(MPI_COMM_WORLD, size)
-  print '(2(a,i2),a)', "Hello parallel world from process nr", rank, " out of", size, "!"
-  call mpi_finalize()
+  call mpi_comm_rank(MPI_COMM_WORLD, rank, rc)
+  call mpi_comm_size(MPI_COMM_WORLD, size, rc)
+  call mpi_barrier(MPI_COMM_WORLD, rc)
+  print '(2(a,i3),a)', "Hello parallel world from process nr", rank, " out of", size, "!"
+  call mpi_finalize(rc)
 end program helloworld

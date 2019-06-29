64f464381ae825ca06ae8ca6028a3ba46cc6e370
Moorits Mihkel Muru <mooritsmihkel@gmail.com>
2019-06-29 15:40:40 +0300

    finished up non-blocking exercises

--- mpi/non-blocking/fortran/msg-chain_presistent.f90
+++ mpi/non-blocking/fortran/msg-chain_presistent.f90
@@ -4,7 +4,7 @@ program basic
 
   implicit none
   integer, parameter :: msgsize = 10000000
-  integer :: rc, myid, ntasks
+  integer :: rc, myid, ntasks, i
   integer :: message(msgsize)
   integer :: receiveBuffer(msgsize)
   type(mpi_status) :: status(2)
@@ -37,19 +37,29 @@ program basic
   call mpi_barrier(mpi_comm_world, rc)
   t0 = mpi_wtime()
 
-  ! TODO: Implement the message passing using non-blocking
-  !       sends and receives
-  call mpi_isend(message, msgsize, mpi_integer, destination, myid+1, mpi_comm_world, requests(1), rc)
-  call mpi_irecv(receiveBuffer, msgsize, mpi_integer, source, myid, mpi_comm_world, requests(2), rc)
-  call mpi_waitall(2, requests, status, rc)
+
+  requests(1) = mpi_request_null
+  requests(2) = mpi_request_null
+
+  ! Bonus exercise to use presistent communication
+
+  call mpi_recv_init(receiveBuffer, msgsize, mpi_integer, source, myid, mpi_comm_world, requests(2), rc)
+  call mpi_send_init(message, msgsize, mpi_integer, destination, myid+1, &
+                     mpi_comm_world, requests(1), rc)
+  do i = 1,100
+    call mpi_start(requests(2), rc)
+    call mpi_start(requests(1), rc)
+    
+
+    call mpi_wait(requests(1), mpi_status_ignore, rc)
+    call mpi_wait(requests(2), mpi_status_ignore, rc)
+  end do 
 
   call mpi_get_count(status(1), MPI_INTEGER, count, rc)
   write(*,'(A10,I3,A20,I8,A,I3,A,I3)') 'Sender: ', myid, &
        ' Sent elements: ', count,  &
        '. Tag: ', myid + 1, '. Receiver: ', destination
 
-  ! TODO: Add here a synchronization call so that you can be sure
-  !       that the message has been received
 
 
   call mpi_get_count(status(2), MPI_INTEGER, count, rc)
@@ -58,8 +68,6 @@ program basic
        '. Tag: ', status(2)%MPI_TAG, &
        '. Sender:   ', status(2)%MPI_SOURCE
 
-  call mpi_request_free(requests, rc)
-
   ! Finalize measuring the time and print it out
   t1 = mpi_wtime()
   call mpi_barrier(mpi_comm_world, rc)
@@ -67,7 +75,8 @@ program basic
 
   write(*, '(A20, I3, A, F6.3)') 'Time elapsed in rank', myid, ':', t1-t0
 
-
+  call mpi_request_free(requests(1), rc)
+  call mpi_request_free(requests(2), rc)
 
 
 

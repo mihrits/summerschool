3c963f596a0cc5263b713feb839d29325ef1ffbf
Moorits Mihkel Muru <mooritsmihkel@gmail.com>
2019-07-01 15:19:18 +0300

    openmp workload sharing wokring code

--- hybrid/work-sharing/sum.F90
+++ hybrid/work-sharing/sum.F90
@@ -7,6 +7,7 @@ program vectorsum
   real(kind=rk), dimension(nx) :: vecA, vecB, vecC
   real(kind=rk)    :: sum
   integer(kind=ik) :: i
+  real(rk) :: t1, t2
 
   ! Initialization of vectors
   do i = 1, nx
@@ -14,11 +15,19 @@ program vectorsum
      vecB(i) = vecA(i)**2
   end do
 
+  call cpu_time(t1)
+
   ! TODO:
   !   Implement here the parallelized version of vector addition,
   !   vecC = vecA + vecB
+  !$omp parallel do schedule(dynamic)
+  do i = 1, nx
+    vecC(i) = vecA(i) + vecB(i)
+  end do
+  !$omp end parallel do
 
+  call cpu_time(t2)
   ! Compute the check value
-  write(*,*) 'Reduction sum: ', sum(vecC)
+  write(*,*) 'Reduction sum: ', sum(vecC), '. Calculation time: ', t2-t1
 
 end program vectorsum

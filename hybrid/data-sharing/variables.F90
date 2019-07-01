f1bba2bf217419842e22b8eae8ebe9bd7d17f3d3
Moorits Mihkel Muru <mooritsmihkel@gmail.com>
2019-07-01 14:39:22 +0300

    variable testing in openmp working code

--- hybrid/data-sharing/variables.F90
+++ hybrid/data-sharing/variables.F90
@@ -5,11 +5,13 @@ program exer1
   var2 = 2
 
   ! TODO:
+  !$omp parallel firstprivate(var1, var2)
   !   Test different data sharing clauses here
   print *, 'Region 1:       var1=', var1, 'var2=', var2
   var1 = var1 + 1
   var2 = var2 + 1
   !end here :)
+  !$omp end parallel
   print *, 'After region 1: var1=', var1, 'var2=', var2
   print *
 

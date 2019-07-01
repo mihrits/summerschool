920ae531ceb90c03de556310204902e73b76f7dc
Moorits Mihkel Muru <mooritsmihkel@gmail.com>
2019-07-01 14:21:43 +0300

    hybrid hello world working code. And hello to you as well
    q
    
    n
    exit
    q

--- hybrid/hello-world/skeleton.F90
+++ hybrid/hello-world/skeleton.F90
@@ -6,11 +6,21 @@ program hello
   integer :: provided, required=MPI_THREAD_FUNNELED
 
   ! TODO: Initialize MPI with thread support.
+  call mpi_init_thread(required, provided, rc)
+
+  call mpi_comm_rank(mpi_comm_world, my_id, rc)
+
+  write(*,*) 'Hello from the rank ', my_id, '!'
+  !$omp parallel private(tid)
+    
 
   ! TODO: Find out the MPI rank and thread ID of each thread and print
   !       out the results.
+  tid  = omp_get_thread_num()
+  write(*,*) 'Hello from process ', my_id, ', thread ', tid
 
   ! TODO: Investigate the provided thread support level.
-
+  write(*,*) 'Support level is ', provided
+  !$omp end parallel
   call MPI_Finalize(rc)
 end program hello

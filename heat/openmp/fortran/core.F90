d1b62ae752ebb2cfdc4dd131cecb117c98d8808a
Moorits Mihkel Muru <mooritsmihkel@gmail.com>
2019-07-02 14:13:39 +0300

    worked on openmp heat programm, has a bug, can't get field data

---
+++ heat/openmp/fortran/core.F90
@@ -0,0 +1,37 @@
+! Main solver routines for heat equation solver
+module core
+
+contains
+
+  ! Compute one time step of temperature evolution
+  ! Arguments:
+  !   curr (type(field)): current temperature values
+  !   prev (type(field)): values from previous time step
+  !   a (real(dp)): update equation constant
+  !   dt (real(dp)): time step value
+  subroutine evolve(curr, prev, a, dt)
+
+    use heat
+    implicit none
+
+    type(field), intent(inout) :: curr, prev
+    !real(dp), allocatable :: temp(:,:)
+    real(dp) :: a, dt
+    integer :: i, j, nx, ny
+
+    nx = curr%nx
+    ny = curr%ny
+
+    ! Calculating the next step of heat equation
+    !$omp do collapse(2)
+    do i = 1, ny
+      do j = 1, nx
+        curr%data(i, j) = prev%data(i, j) + dt * a &
+                        * ((prev%data(i-1,j) - 2*prev%data(i,j) + prev%data(i+1,j))/curr%dy**2 &
+                        +  (prev%data(i,j-1) - 2*prev%data(i,j) + prev%data(i,j+1))/curr%dx**2)
+      end do
+    end do
+    !$omp end do
+  end subroutine evolve
+
+end module core

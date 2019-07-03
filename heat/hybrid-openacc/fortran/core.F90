cd5db6a06d80472b1aad58063486fd24d6c9aa5f
Moorits Mihkel Muru <mooritsmihkel@gmail.com>
2019-07-03 10:20:35 +0300

    added basic openacc to heat

--- heat/hybrid-openacc/fortran/core.F90
+++ heat/hybrid-openacc/fortran/core.F90
@@ -58,6 +58,7 @@ contains
     prevdata => prev%data
 
     ! TODO: use OpenACC to parallelise the loops
+    !$acc parallel loop collapse
     do j = 1, ny
        do i = 1, nx
           currdata(i, j) = prevdata(i, j) + a * dt * &
@@ -67,6 +68,7 @@ contains
                &   prevdata(i, j+1)) / dy**2)
        end do
     end do
+    !$acc end parallel loop
   end subroutine evolve
 
 end module core
